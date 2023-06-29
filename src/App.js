f0b7e13cd7e18e0af580e696cf4a966387023e51
Santajit Patra <business.santajitpatra@gmail.com>
2023-06-29 18:12:08 +0530

    feat : redux old
    How  to use redux like  context API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch } from "react-redux";
+import Count from "./components/Count";
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={(e) => dispatch({ type: "INCREMENT" })}>Increase</button>
+      <Count />
+      <button onClick={(e) => dispatch({ type: "DECREMENT" })}>Decrease</button>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/Count.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const Count = () => {
+const count = useSelector(state => state)
+
+  return (
+    <div>{count}</div>
+  )
+}
+
+export default Count
